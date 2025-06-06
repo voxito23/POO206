a1e45d96d135d70e5e4a71af828afc4a5f6a2ffe
voxito23 <victorrodher493@gmail.com>
2025-06-05 20:09:14 -0600

    Palabras descomponer

---
+++ ExamenPrimerParcial/palabralarga.py
@@ -0,0 +1,16 @@
+while True:
+    entrada = input("Introduce una palabra o 'v' para salir: ")
+    if entrada.lower() == 'v':
+        print("Programa terminado por el usuario.")
+        break
+    try:
+        palabra1 = entrada.strip().split()
+        p1 = len (palabra1)
+        if len(palabra1) == 1:
+            palabra = palabra1[1]
+            for i, letra in enumerate(palabra, 1):
+                print(f"letra {i}: {letra}")
+        else:
+            print("Por favor introduce solo una palabra")
+    except ValueError:
+        print("Por favor introduce una palabra")

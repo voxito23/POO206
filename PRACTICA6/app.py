1e5e46bfd7800531fb46439d313daa15afec6d88
voxito23 <victorrodher493@gmail.com>
2025-06-11 18:39:58 -0600

    Hola Mundo Flask

---
+++ PRACTICA6/app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hola Mundo FLASK ;)'
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
+    
